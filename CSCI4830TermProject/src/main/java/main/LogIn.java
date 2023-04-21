eeac0b9244b5d8da44089cb42761fd2e18e121d4
ckachek <87252700+ckachek@users.noreply.github.com>
2023-04-20 21:02:00 -0500

    Full functionality for Login page added (checking against registered users, taking back to login page if wrong credentials), initial insert functionality for Signup added

--- CSCI4830TermProject/src/main/java/main/LogIn.java
+++ CSCI4830TermProject/src/main/java/main/LogIn.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -33,30 +34,46 @@ public class LogIn extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String user = request.getParameter("uname");
 		String pass = request.getParameter("pwd");
+		PrintWriter out = response.getWriter();
 		
-		//TODO: prepared statement on table of users&passwords to see if match. If no match redirect back to login page
 		Connection connection = null;
 	      PreparedStatement preparedStatement = null;
 	      try {
 	         DBConnection.getDBConnection(getServletContext());
 	         connection = DBConnection.connection;
 	         
-	         //TODO: no user database yet, fix below stuff to run properly
-	         /*
-	         String selectSQL = "SELECT * FROM users WHERE USER LIKE ? AND PASSWORD LIKE ?";
+	         //select all instances of entered username & password, since only one of each username there should just be 1 or 0 
+	         String selectSQL = "SELECT * FROM user WHERE username LIKE ? AND password LIKE ?";
 	         preparedStatement = connection.prepareStatement(selectSQL);
 	         preparedStatement.setString(1, user);
 	         preparedStatement.setString(2, pass);
+	         
 	         ResultSet rs = preparedStatement.executeQuery();
 	         
-	         if (!rs.next()) {
-	        	 response.sendRedirect("LogIn.html");
-	         }*/
+	         if (rs.next()) {	//if rs.next() is true it means that there is a matches with the database table
+	        	 //if match, set user variable and redirect to home page
+	        	 request.getSession().setAttribute("user", user);	//set the user name as a session variable so other servlets can use
+	        	 //String user = (String)request.getSession().getAttribute("user"); How to get the session variable
+	        	 response.sendRedirect("HomePage.html");	//go to home page upon successful sign in
+	        	 
+	         }
 	         
-	         //if match, set user variable and redirect to home page
-	         request.getSession().setAttribute("user", user);	//set the user name as a session variable so other servlets can use
-	         //String user = (String)request.getSession().getAttribute("user"); How to get the session variable
-	         response.sendRedirect("HomePage.html");	//go to home page upon successful sign in
+	         //Display a page indicating username or password is incorrect with a link back to login page
+	         String title = "Login Failed";
+	         String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+	         out.println(docType + //
+	               "<html>\n" + //
+	               "<head><title>" + title + "</title></head>\n" + //
+	               "<body bgcolor=\"#f0f0f0\">\n" + //
+	               "<h2 align=\"center\">" + title + "</h2>\n" + //
+	               "<ul>\n" + //
+
+	               " <li> Username or Password incorrect. Try Again.\n" + //
+
+	               "</ul>\n");
+
+	          out.println("<a href=/CSCI4830TermProject/LogIn.html>Return to login</a> <br>");
+	          out.println("</body></html>");
 	         
 	         connection.close();        
 	      } catch (Exception e) {
--- CSCI4830TermProject/src/main/java/main/SignUp.java
+++ CSCI4830TermProject/src/main/java/main/SignUp.java
@@ -2,12 +2,16 @@ package main;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import util.DBConnection;
 
 @WebServlet("/SignUp")
 public class SignUp extends HttpServlet {
@@ -24,7 +28,27 @@ public class SignUp extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//TODO: set up prepared statement to enter new user into table
+		String user = request.getParameter("uname");
+		String pass = request.getParameter("pwd");
+		
+		Connection connection = null;
+		String signupInsert = "INSERT INTO user (username, password) values (?, ?)";
+		//TODO: don't use prepared statement if username already exists in database
+		//create some sort of message to indicate failure and redirect to home screen
+		//maybe use a statement that checks if the username exists in the table as the if condition
+		//if either username or password are blank automatically fail, display message and send back to sign up page 
+		try {
+			DBConnection.getDBConnection(getServletContext());
+	        connection = DBConnection.connection;
+	        //If username and password are both filled in and valid
+	        PreparedStatement preparedStmt = connection.prepareStatement(signupInsert);
+	        preparedStmt.setString(1, user);
+	        preparedStmt.setString(2, pass);
+	        preparedStmt.execute();
+	        connection.close();
+		} catch (Exception e) {
+	        e.printStackTrace();
+	    }
 		
 		//TODO: set up page with boxes for birthday submission and button to redirect to login page
 		response.setContentType("text/html");
